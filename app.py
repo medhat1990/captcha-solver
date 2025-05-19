7308ee7e6060fb63755f41ca5a884327ddb06993
medhat1990 <136806134+medhat1990@users.noreply.github.com>
2025-05-19 08:52:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ def predict():
 def index():
     return "✅ Model is up and running!"
 
-# ⬅️ هذا السطر هو المفتاح للتشغيل على Railway
 if __name__ == "__main__":
+    from waitress import serve
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    serve(app, host="0.0.0.0", port=port)
